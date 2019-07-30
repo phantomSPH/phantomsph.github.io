da12ab66c4022c1446079afd26aa932e13b27105
Daniel Price <daniel.price@monash.edu>
2019-07-31 09:43:59 +1000

    [nightly]: results Wed Jul 31 09:43:59 AEST 2019

--- nightly/opt/polar.js
+++ nightly/opt/polar.js
@@ -5,8 +5,6 @@
 
       var data = new google.visualization.DataTable();
       data.addColumn('date', 'Date');
-      data.addColumn('number', 'time-gfortran-16');
-      data.addColumn('number', 'time-msg-16');
       data.addRows([
     [new Date(2019,01,22,12,26,26),41.28,38.91],
     [new Date(2019,01,22,13,28,05),41.06,38.60],
