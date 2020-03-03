2eba43b9a9d7e36c17693c3ef81bade6b7464e9e
Daniel Price <daniel.price@monash.edu>
2020-03-03 21:41:51 +1100

    [nightly]: results Tue Mar  3 21:41:51 AEDT 2020

--- nightly/opt/passy.js
+++ nightly/opt/passy.js
@@ -11,6 +11,7 @@
       data.addColumn('number', 'time-msg-16');
       data.addRows([
     [new Date(2020,02,02,21,32,10),32.82,32.89],
+    [new Date(2020,02,03,21,30,41),36.90,36.35],
       ]);
 
       var options = {
