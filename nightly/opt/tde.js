ad2e49065e18dac2be89e299de0c39c89aeedbf1
Conrad Chan <ctchan@swin.edu.au>
2021-07-26 08:23:52 +0000

    [benchmarks]: tde 2021-07-26T08:23:51

---
+++ nightly/opt/tde.js
@@ -0,0 +1,26 @@
+    google.charts.load('current', {'packages':['line']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'Date');
+      data.addColumn('number', 'time-gfortran-4');
+      data.addColumn('number', 'time-ifort-4');
+      data.addRows([
+    [new Date(2021,06,26,08,08,39),425.71,777.34],
+      ]);
+
+      var options = {
+       chart: {
+         title: 'Benchmark timings for tde test',
+         subtitle: 'Performed on phantom-runner-4'
+        },
+        width: 900,
+        height: 500
+      };
+
+      var chart = new google.charts.Line(document.getElementById('tde'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
